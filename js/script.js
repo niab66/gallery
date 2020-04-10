fd664a84aed97d404e7c0fda0ab1111f6402fa34
Kai Bailey <kbailey1@ualberta.ca>
2020-04-09 19:14:33 -0600

    Resolve renderer bug

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
 var id = 'gallery';
 
 function reqListener() {
-  var renderer
-  switch (layoutStyle) {
-    case COLUMNS:
-      renderer = new VerticalRenderer(id);
-      break;
-    case ROWS:
-      renderer = new HorizontalRenderer(id);
-      break;
-    case SQUARES:
-      renderer = new SquareRenderer(id);
-      break;
-  }
+  var renderer = new VerticalRenderer(id);
   var config = new Config(JSON.parse(this.responseText), configuration);
   renderer.render(config);
   lazyload();
 }
 
 
-window.onload = function() {
+window.onload = function () {
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", "config.json");
